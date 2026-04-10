a5b03b3ea1ec011bcaafa6264fc004140bff21ed
Marcos Davi de Souza <marcosdavi.contato@gmail.com>
2026-04-10 13:38:59 -0300

    Day one exercice, if and elses

---
+++ Day 1: If Else Statements.js
@@ -0,0 +1,17 @@
+if (marks > 90){
+    console.log('AA')
+} else if (80 < marks){
+    console.log('AB')
+} else if (70 < marks){
+    console.log('BB')
+} else if (60 < marks){
+    console.log('BC')
+} else if (50 < marks){
+    console.log('CC')
+} else if (40 < marks){
+    console.log('CD')
+} else if (30 < marks){
+    console.log('DD')
+} else if (marks <= 30){
+    console.log('FF')
+}
