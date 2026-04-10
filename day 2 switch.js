2bb51572928f5e769b08f419ccdf011473ae38a0
Marcos Davi de Souza <marcosdavi.contato@gmail.com>
2026-04-10 13:52:36 -0300

    Correct typos and breaks in switch statement
    Fix typos in case statements and ensure proper breaks.

---
+++ day 2 switch.js
@@ -0,0 +1,33 @@
+switch (num){
+  case 1:
+    console.log('ONE');
+    break;
+  case 2:
+    console.log('TWO');
+    break;
+  case 3:
+    console.log('TREE');
+    brak;
+  case 4:
+    console.log('FOUR');
+    break;
+  case 5:
+    console.log('FIVE');
+    brak;
+  case 6:
+    console.log('SIX');
+    break;
+  case 7:
+    console.log('SEVEN');
+    break;
+  case 8:
+    console.log('EIGTH');
+    brak;
+  case 9:
+    console.log('NINE');
+    brak;
+  default:
+    console.log('PLEASE TRY AGAIN');
+    break;
+}
+  
